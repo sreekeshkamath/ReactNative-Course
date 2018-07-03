ad420097faeff96e67671c29fc480db84b4d15dc
Sreekesh Kamath <sreekeshkamath95@gmail.com>
2018-07-03 21:45:45 +0530

    New Collision 2

--- rn-course-1/App.js
+++ rn-course-1/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Loadedasd last commit asd ads collision 1</Text>
+        <Text>Loadedasd last commit asd ads collision 2</Text>
         <PlaceDetail
           selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
