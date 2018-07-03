58fc21f6a1957751827472b004ca61548bf03148
Sreekesh Kamath <sreekeshkamath95@gmail.com>
2018-07-03 19:07:08 +0530

    New Branch Commit 2

--- rn-course-1/App.js
+++ rn-course-1/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Loadedasd last commit asdasd</Text>
+        <Text>Loadedasd last commit asdasd asd</Text>
         <PlaceDetail
           selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
