41900b1a251858a6fa35efc6b3daaed724e7ef89
Sreekesh Kamath <sreekeshkamath95@gmail.com>
2018-07-03 18:20:37 +0530

    Push Multiple Files

--- rn-course-1/src/components/ListItem/ListItem.js
+++ rn-course-1/src/components/ListItem/ListItem.js
@@ -5,7 +5,7 @@ const listItem = props => (
   <TouchableOpacity onPress={props.onItemPressed}>
     <View style={styles.listItem}>
       <Image source={props.placeImage} style={styles.placeImage} />
-      <Text>{props.placeName}</Text>
+      <Text>{props.placeName}sd</Text>
     </View>
   </TouchableOpacity>
 );
