62542e1508456515121e6f95f80fb31df024025d
Sreekesh Kamath <sreekeshkamath95@gmail.com>
2018-07-03 18:57:00 +0530

    new branch changes

--- rn-course-1/App.js
+++ rn-course-1/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Loadedasd last commit asd</Text>
+        <Text>Loadedasd last commit asd ads</Text>
         <PlaceDetail
           selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
